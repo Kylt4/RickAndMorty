b70db6f77be154a6a5882599ad6fe10ac302754b
christophe.bugnon <christophe@wemoms.fr>
2025-06-28 13:45:32 +0200

    [EPISODE] - load command delivers error on client error

--- RickAndMorty/RickAndMortyTests/EpisodeLoaderTests.swift
+++ RickAndMorty/RickAndMortyTests/EpisodeLoaderTests.swift
@@ -10,8 +10,25 @@ import XCTest
 class HTTPClient {
     var urls = [URL]()
 
-    func get(from url: URL) {
+    var getContinuation: CheckedContinuation<Void, Error>?
+
+    func get(from url: URL) async throws  {
         urls.append(url)
+        return try await withCheckedThrowingContinuation { continuation in
+            getContinuation = continuation
+        }
+    }
+
+    func completeSuccessfully() async {
+        try? await Task.sleep(nanoseconds: 1_000_000)
+        getContinuation?.resume(with: .success(()))
+        getContinuation = nil
+    }
+
+    func completeWith(error: Error) async {
+        try? await Task.sleep(nanoseconds: 1_000_000)
+        getContinuation?.resume(throwing: error)
+        getContinuation = nil
     }
 }
 
@@ -24,8 +41,8 @@ class EpisodeLoader {
         self.client = client
     }
 
-    func load() {
-        client.get(from: url)
+    func load() async throws {
+        try await client.get(from: url)
     }
 }
 
@@ -37,26 +54,55 @@ class EpisodeLoaderTests: XCTestCase {
         XCTAssertTrue(spy.urls.isEmpty)
     }
 
-    func test_load_requestGetFromURL() {
+    func test_load_requestGetFromURL() async {
         let anyURL = anyURL()
         let (sut, spy) = makeSUT(url: anyURL)
 
-        sut.load()
+        performLoadTask(from: sut)
+        await spy.completeSuccessfully()
 
         XCTAssertEqual(spy.urls, [anyURL])
     }
 
-    func test_loadTwice_requestGetFromURLTwice() {
+    func test_loadTwice_requestGetFromURLTwice() async {
         let anyURL = anyURL()
         let (sut, spy) = makeSUT(url: anyURL)
 
-        sut.load()
-        sut.load()
+        performLoadTask(from: sut)
+        await spy.completeSuccessfully()
+        performLoadTask(from: sut)
+        await spy.completeSuccessfully()
+
 
         XCTAssertEqual(spy.urls, [anyURL, anyURL])
     }
 
+
+    func test_load_deliversErrorOnClientError() async {
+        let anyURL = anyURL()
+        let (sut, spy) = makeSUT(url: anyURL)
+        let clientError = NSError(domain: "client error", code: 0)
+
+        let task = performLoadTask(from: sut)
+        await spy.completeWith(error: clientError)
+
+        do {
+            try await task.value
+            XCTFail("Expected \(clientError) error, got success instead")
+        } catch {
+            print("[DEBUG] error", error)
+            XCTAssertEqual(error as NSError, clientError)
+        }
+    }
+
     // MARK: - helpers
+
+    @discardableResult
+    private func performLoadTask(from sut: EpisodeLoader) -> Task<Void, Error> {
+        Task {
+            try await sut.load()
+        }
+    }
 
     private func makeSUT(url: URL = URL(string: "http://any-url.com")!) -> (sut: EpisodeLoader, spy: HTTPClient) {
         let spy = HTTPClient()
