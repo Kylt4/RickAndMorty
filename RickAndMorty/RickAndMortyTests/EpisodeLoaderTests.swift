fccdab7d93099c32d6300903d3a1b6e27217efa1
christophe.bugnon <christophe@wemoms.fr>
2025-06-28 13:26:46 +0200

    [EPISODE] - load command request get from url

--- RickAndMorty/RickAndMortyTests/EpisodeLoaderTests.swift
+++ RickAndMorty/RickAndMortyTests/EpisodeLoaderTests.swift
@@ -9,18 +9,42 @@ import XCTest
 
 class HTTPClient {
     var urls = [URL]()
+
+    func get(from url: URL) {
+        urls.append(url)
+    }
 }
 
 class EpisodeLoader {
+    private let url: URL
+    private let client: HTTPClient
 
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    func load() {
+        client.get(from: url)
+    }
 }
 
 class EpisodeLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestGetFromURL() {
         let spy = HTTPClient()
-        let _ = EpisodeLoader()
+        let _ = EpisodeLoader(url: URL(string: "http://any-url.com")!, client: spy)
 
         XCTAssertTrue(spy.urls.isEmpty)
+    }
+
+    func test_load_requestGetFromURL() {
+        let anyURL = URL(string: "http://any-url.com")!
+        let spy = HTTPClient()
+        let sut = EpisodeLoader(url: anyURL, client: spy)
+
+        sut.load()
+
+        XCTAssertEqual(spy.urls, [anyURL])
     }
 }
